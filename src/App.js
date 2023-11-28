115c3f13d8063a1151d6911f9f13117345cfc4c3
Abhishek Gajera <abhishekgajera3140@gmail.com>
2023-11-28 13:24:15 +0530

    feat: batter UI and error handling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import { useState, useMemo } from 'react'
-import { FilesViewer } from './FilesViewer'
+import React, { useState, useMemo, useEffect } from 'react';
+import { FilesViewer } from './FilesViewer';
+import { IconFolder, IconFile, IconFolderOpen } from './Icons';
 
-const fs = window.require('fs')
-const fsExtra = window.require('fs-extra')
-const pathModule = window.require('path')
-
-const { app } = window.require('@electron/remote')
+const fs = window.require('fs');
+const fsExtra = window.require('fs-extra');
+const pathModule = window.require('path');
+const { app, dialog } = window.require('@electron/remote');
 
 const formatSize = size => {
-  var i = Math.floor(Math.log(size) / Math.log(1024))
+  var i = Math.floor(Math.log(size) / Math.log(1024));
   return (
     (size / Math.pow(1024, i)).toFixed(2) * 1 +
     ' ' +
     ['B', 'kB', 'MB', 'GB', 'TB'][i]
-  )
-}
+  );
+};
 
 function App() {
-  const [path, setPath] = useState(app.getAppPath())
+  const defaultDestinationPath = pathModule.join(
+    app.getAppPath(),
+    'src',
+    'tally_data'
+  );
+  const [path, setPath] = useState(app.getAppPath());
+  const [destinationPath, setDestinationPath] = useState(
+    defaultDestinationPath
+  );
+
+  useEffect(() => {
+    setDestinationPath(defaultDestinationPath);
+  }, [defaultDestinationPath]);
+
+  const [syncing, setSyncing] = useState(false);
+  const [syncMessage, setSyncMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [searchString, setSearchString] = useState('');
 
   const files = useMemo(() => {
     try {
@@ -30,11 +47,11 @@ function App() {
             return {
               name: file,
               size: stats.isFile() ? formatSize(stats.size ?? 0) : null,
-              directory: stats.isDirectory()
+              directory: stats.isDirectory(),
             };
           } catch (error) {
             console.error(`Error reading stats for file ${filePath}:`, error);
-            return null; // or handle the error in a way that makes sense for your application
+            return null;
           }
         })
         .filter(file => file !== null);
@@ -44,46 +61,176 @@ function App() {
     }
   }, [path]);
 
-  const onBack = () => setPath(pathModule.dirname(path))
-  const onOpen = folder => setPath(pathModule.join(path, folder))
+  const onBack = () => setPath(pathModule.dirname(path));
+  const onOpen = folder => setPath(pathModule.join(path, folder));
 
-  const [searchString, setSearchString] = useState('')
-  const filteredFiles = files.filter(s => s.name.startsWith(searchString))
+  const filteredFiles = files.filter(s =>
+    s.name.toLowerCase().startsWith(searchString.toLowerCase())
+  );
+
+  const handlePathChange = async newPath => {
+    try {
+      await fsExtra.access(
+        newPath,
+        fsExtra.constants.R_OK | fsExtra.constants.W_OK
+      );
+      setPath(newPath);
+    } catch (error) {
+      console.error('Error accessing the selected path:', error);
+    }
+  };
+
+  const openPathDialog = async () => {
+    const result = await dialog.showOpenDialog({
+      properties: ['openDirectory'],
+      title: 'Select TALLY_DATA_PATH',
+    });
+
+    if (!result.canceled && result.filePaths.length > 0) {
+      const selectedPath = result.filePaths[0];
+      await handlePathChange(selectedPath);
+    }
+  };
+
+  const handleDestinationPathChange = async newPath => {
+    try {
+      await fsExtra.access(
+        newPath,
+        fsExtra.constants.R_OK | fsExtra.constants.W_OK
+      );
+      setDestinationPath(newPath);
+    } catch (error) {
+      console.error('Error accessing the selected destination path:', error);
+    }
+  };
+
+  const openDestinationPathDialog = async () => {
+    const result = await dialog.showOpenDialog({
+      properties: ['openDirectory'],
+      title: 'Select Destination Path',
+    });
+
+    if (!result.canceled && result.filePaths.length > 0) {
+      const selectedPath = result.filePaths[0];
+      await handleDestinationPathChange(selectedPath);
+    }
+  };
 
   const syncData = async () => {
-    const TALLY_DATA_PATH = 'C:\\Users\\Public\\TallyPrimeEditLog\\data'; // Replace with your actual path
-    const destinationPath = pathModule.join(app.getAppPath(), 'src', 'tally_data');
-  
+    setSyncing(true);
+    setSyncMessage('');
+    setErrorMessage('');
+
+    const TALLY_DATA_PATH = path;
+
+    if (!TALLY_DATA_PATH) {
+      const result = await dialog.showOpenDialog({
+        properties: ['openDirectory'],
+        title: 'Select TALLY_DATA_PATH',
+      });
+
+      if (result.canceled || result.filePaths.length === 0) {
+        console.warn('User canceled path selection.');
+        setSyncing(false);
+        return;
+      }
+
+      const selectedPath = result.filePaths[0];
+      setPath(selectedPath);
+    }
+
     try {
-      // Ensure the destination directory exists
       await fsExtra.ensureDir(destinationPath);
-  
-      // Copy data from TALLY_DATA_PATH to the destination path
       await fsExtra.copy(TALLY_DATA_PATH, destinationPath, { overwrite: true });
-  
-      console.info('Data synced successfully.');
+      setSyncMessage('Data synced successfully.');
     } catch (error) {
       console.error('Error syncing data:', error);
+      setErrorMessage(`Error syncing data: ${error.message}`);
+    } finally {
+      setSyncing(false);
     }
   };
 
   return (
-    <div className="container mt-2">
-      <button onClick={syncData} className="btn btn-primary mt-2">
-        Sync Data
-      </button>
-      <h4>{path}</h4>{console.info("path++ ", path)}
-      <div className="form-group mt-4 mb-2">
-        <input
-          value={searchString}
-          onChange={event => setSearchString(event.target.value)}
-          className="form-control form-control-sm"
-          placeholder="File search"
-        />
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="form-group mb-3">
+            <label htmlFor="pathInput" className="form-label">
+              TALLY_DATA_PATH:
+            </label>
+            <div className="input-group">
+              <input
+                id="pathInput"
+                type="text"
+                className="form-control"
+                value={path}
+                onChange={e => handlePathChange(e.target.value)}
+              />
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={openPathDialog}
+              >
+                Browse
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="form-group mb-3">
+            <label htmlFor="destinationPathInput" className="form-label">
+              Destination Path:
+            </label>
+            <div className="input-group">
+              <input
+                id="destinationPathInput"
+                type="text"
+                className="form-control"
+                value={destinationPath}
+                onChange={e => handleDestinationPathChange(e.target.value)}
+              />
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={openDestinationPathDialog}
+              >
+                Browse
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <button
+        onClick={syncData}
+        className="btn btn-primary mt-2"
+        disabled={syncing}
+      >
+        {syncing ? 'Syncing...' : 'Sync Data'}
+      </button>
+
+      {syncMessage && (
+        <div className="alert alert-success mt-3">{syncMessage}</div>
+      )}
+      {errorMessage && (
+        <div className="alert alert-danger mt-3">{errorMessage}</div>
+      )}
+
+      <div className="row mt-4">
+        <div className="col-md-6">
+          <input
+            value={searchString}
+            onChange={event => setSearchString(event.target.value)}
+            className="form-control form-control-sm"
+            placeholder="File search"
+          />
+        </div>
+      </div>
+
       <FilesViewer files={filteredFiles} onBack={onBack} onOpen={onOpen} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
